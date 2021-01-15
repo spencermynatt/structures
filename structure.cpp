44b9ed8232065165bdfc27432de0b2c4ce1e3e94
spencermynatt <75866352+spencermynatt@users.noreply.github.com>
2021-01-15 10:46:37 -0600

    Add files via upload

---
+++ structure.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+struct obj {
+	int height;
+	int wingspan;
+	int weight;
+
+
+
+};
+
+int main() {
+
+	obj sizes;
+	
+	
+	cout << "enter your height";
+	cin >> (sizes).height;
+
+	cout << (sizes).height << endl;
+
+	return 0;
+}
